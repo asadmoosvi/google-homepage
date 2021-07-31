326a16b65c5d4f6bbce1a10db920c891b1615ca3
Asad Moosvi <moosvi.asad@gmail.com>
2021-07-31 16:46:28 +0500

    Add anmation on lucky button click

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,17 @@ function googleSearch(query) {
 
 const form = document.querySelector('.search__form');
 const query = document.querySelector('.search__input');
+const luckyButton = document.querySelector('#lucky-button');
+const searchImage = document.querySelector('.search__image');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   googleSearch(query.value);
+});
+
+let imageRotateAngle = 0;
+luckyButton.addEventListener('click', () => {
+  imageRotateAngle += 360;
+  searchImage.style.transition = 'transform 600ms ease-in-out';
+  searchImage.style.transform = `rotate(${imageRotateAngle}deg)`;
 });
